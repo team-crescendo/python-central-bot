03207cfca505e1efc259c047cbfce42cf2b66ef6
return0927 <bjleh0927@gmail.com>
2018-08-11 00:54:26 +0900

    * 2018.08.11 Removed inappropriate type-definition

--- cogs/utils/users.py
+++ cogs/utils/users.py
@@ -3,11 +3,13 @@ from discord import User
 def get_uuid(db, user):
     if len(user) == 36:
         _, data = db.rnf(f"SELECT discord_id FROM discord WHERE uuid='{user}';")
+        print(" Fetched using uuid ", user, ", and data as: ", data)
 
         if data:
             return data[0][0], user
     else:
         _, data = db.rnf(f"SELECT uuid FROM discord WHERE discord_id='{user}';")
+        print(" Fetched using dsid ", user, ", and data as: ", data)
 
         if data:
             return user, data[0][0]
@@ -26,7 +28,8 @@ def get_point(db, user):
     else:
         return None
 
-def parse_mention(user: str):
+def parse_mention(user):
+    print(user)
     if isinstance(user, User):
         user = user.id
     if user[:2] == "<@" and user[-1:] == ">":
