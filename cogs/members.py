8470cd9a996ffbb1cd95c8d2e32a633d69588cc0
return0927 <admin@return0927.xyz>
2018-08-10 11:55:56 +0900

    * 2018.08.10 Updated members.py
     - Added `탈퇴` command for unregistration

--- cogs/members.py
+++ cogs/members.py
@@ -1,8 +1,9 @@
-from discord.ext.commands import command, CheckFailure
+from discord import User
+from discord.ext.commands import command, group, CheckFailure
 from settings import Config
 from db import PostgreSQL
 
-from .utils import checks
+from .utils import checks, users
 
 import i18n
 t = i18n.t
@@ -33,6 +34,10 @@ class Member:
     async def register(self, ctx):
         bot = ctx.bot
 
+        if not checks.is_valid(self.DB, False)(ctx):
+            await bot.reply(t("messages.register_inavailable"))
+            return
+
         Y = t("emojis.t")
         N = t("emojis.f")
 
@@ -56,10 +61,45 @@ class Member:
             else:
                 await bot.reply(t('messages.error_call_admin'))
 
+    @command("탈퇴", pass_context=True)
+    @checks.is_user(DB)
+    @checks.is_valid(DB)
+    async def unregister(self, ctx):
+        bot = ctx.bot
+
+        Y = t("emojis.t")
+        N = t("emojis.f")
+
+        o = await bot.reply(
+            t("messages.unregister_really")
+        )
+
+        await bot.add_reaction(o, Y)
+        await bot.add_reaction(o, N)
+        
+        r = await bot.wait_for_reaction(emoji=[Y, N], timeout=60, message=o, user=ctx.message.author)
+
+        await bot.delete_message(o)
+
+        if r and str(r.reaction.emoji) == Y:
+            _, uuid = users.get_uuid(self.DB, ctx.message.author.id)
+            self.DB.run(f"DELETE FROM attendance WHERE uuid='{uuid}';")
+            _, data = self.DB.rnf(f"UPDATE uuid SET valid=false WHERE uuid='{uuid}' RETURNING uuid;")
+
+            if data:
+                await bot.reply(t('messages.unregister_success'))
+            else:
+                await bot.reply(t('messages.error_call_admin'))
+
     @register.error
     async def register_error(self, error, ctx):
         if isinstance(error, CheckFailure):
             await ctx.bot.reply(t('messages.register_already'))
+
+    @unregister.error
+    async def unregister_error(self, error, ctx):
+        if isinstance(error, CheckFailure):
+            await ctx.bot.reply(t('messages.unregister_inavailable'))
 
 
 def setup(bot):
