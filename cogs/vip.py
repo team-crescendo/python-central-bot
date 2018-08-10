405b78ef3fffabc00100e7391113ce0d762dec80
return0927 <admin@return0927.xyz>
2018-08-10 11:56:26 +0900

    * 2018.08.10 Removed duplicated functions
     - moved to admin.py

--- cogs/vip.py
+++ cogs/vip.py
@@ -1,5 +1,5 @@
 from discord import User
-from discord.ext.commands import command, CheckFailure
+from discord.ext.commands import command, group, CheckFailure
 from db import PostgreSQL
 from settings import Config
 
@@ -36,36 +36,6 @@ class VIP:
             await ctx.bot.reply(t("messages.register_request"))
         else:
             await ctx.bot.reply(t("messages.vip_point").format(point=data[0][0]))
-
-    @command('관리', pass_context=True)
-    @checks.is_crescendo()
-    async def admin(self, ctx, *args):
-        menu, *remains = args
-
-        if menu == "조회":
-            user, *_ = remains
-
-            if isinstance(user, User):
-                user = user.id
-            if user[:2] == "<@" and user[-1:] == ">":
-                user = user[2:-1]
-
-            uuid = users.get_uuid(self.DB, user)
-
-            if uuid is None:
-                await ctx.bot.reply(t("admin.no_uuid_found"))
-                return
-
-            point = users.get_point(self.DB, user)
-            await ctx.bot.reply(t("admin.view_point").format(_target=f"<@{user}> ({user})", _uuid=uuid, _point=point))
-                
-
-    @admin.error
-    async def perm_error(self, error, ctx):
-        if isinstance(error, CheckFailure):
-            await ctx.bot.reply(t("messages.less_permission"))
-
-        print(error)
 
 
 def setup(bot):
